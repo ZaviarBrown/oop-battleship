ae732a24f5145e8f6db38a112705e8065296faaf
Zaviar Brown <brown.zaviar@gmail.com>
2021-03-10 16:20:55 -0800

    just started Phase 3

--- starter/game.js
+++ starter/game.js
@@ -1,12 +1,22 @@
+const Board = require("./board");
+const Player = require("./humanPlayer");
+
 class BattleshipGame {
-  constructor() {
+  constructor(player1, numRows, numCols, numShips) {
     // TODO: Set up constructor to store reference to the humanPlayer and
     // instantiate a new instance of the Board class and set it to this.board.
     // Remember to import your Board class.
+    this.numRows = numRows;
+    this.numCols = numCols;
+    this.numShips = numShips;
+    this.board = new Board(this.numRows, this.numCols, this.numShips);
+    curPlayer = new Player;
   }
 
   playTurn() {
     // TODO: Display the state of the game and ask for the users input.
+    this.board.display();
+    curPlayer.getMove();
   }
 
   displayStatus() {
@@ -19,6 +29,7 @@ class BattleshipGame {
     //     display the final status of the game and end the game. If not, play
     //     another turn. If the move is invalid, ask the player to input a valid
     //     position and play another turn.
+    
   }
 }
 
